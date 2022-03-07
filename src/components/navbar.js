6156402f6d68ed7204c2b3a9f843181180ef6f01
8968harsimranjit <61202199+8968harsimranjit@users.noreply.github.com>
2022-03-06 21:58:40 -0800

    adding Header component & images to public folder

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             Penny Juice
-            <i class='fab fa-typo3' />
+            <i class='fa-regular fa-citrus' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
