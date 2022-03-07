e202ae4618481986e1ad669b9d31b1775e3d328e
8968harsimranjit <61202199+8968harsimranjit@users.noreply.github.com>
2022-03-07 08:15:08 -0800

    adding more content to cards.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
+import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
       <Home />
       <Routes>
         <Route path='/' exact component={Home}/>
+        <Route path='/products' exact component={Products}/>
+        <Route path='/contact' exact component={Contact}/>
+        <Route path='/sign-up' exact component={SignUp}/>
       </Routes>
     </Router>
     </>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -25,17 +25,65 @@ function Cards() {
             <CardItem
               src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeetbQ0NOdRSrgBfz3xY8xQstJMQHxJdk41Q&usqp=CAU'
               text='Get Juice Concentrates in a variety of Flavors'
-              path='/services'
+              para='Get delicious and nutritious 100% fruit juice from Penny Juice of America. We provide 100% blended fruit juice concentrates that are specially made for kids and meet all the requirements of the FDA with regard to subsidy and nutrition.'
+              path='/contact'
             />
             <CardItem
               src='https://previews.123rf.com/images/maridav/maridav1505/maridav150500264/40366249-femme-tenant-bo%C3%AEte-de-livraison-de-bouteilles-de-jus-de-fruits-et-de-l%C3%A9gumes-fra%C3%AEchement-press%C3%A9es-%C3%A0-.jpg'
               text='Fast and Easy service'
-              path='/products'
+              para=' Penny Juice greatly reduces storage and handling requirements. 1 gallon pre-measured mix pitchers available to assure cost control and flavor consistency'
+              path='/contact'
             />
             <CardItem
               src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2015/02/juice-main-1-1451561402.jpg?crop=1xw:0.789xh;center,top&resize=480:*'
               text='Best Quality At Best Price'
-              path='/sign-up'
+              para='15 flavors available including 7 color free flavors. Each case contains 6 - ½ gallon bottles of your choice. We’ve designed them especially for childcare, daycare, preschool, basp and extended daycare.'
+              path='/contact'
+            />
+          </ul>
+          <h1 className="heading_2">Welcome to Our Shop!</h1>
+          <ul className='cards__items'>
+          <CardItem
+              src = "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2011/07/14/Food/Images/food019_1310646961.jpg?t=20170517"
+              text='Green Watermelon'
+              path='/products'
+            />
+            <CardItem
+              src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkVCph1Dgtut3zNNuv9cs2m_KHALVUFLqllQ&usqp=CAU"
+              text='Fresh Orange'
+              path='/products'
+            />
+            <CardItem
+              src = "https://food.unl.edu/recipes/strawberry-banana-smoothie.jpg"
+              text='Strawberry Banana'
+              path='/products'
+            />
+            <CardItem
+              src = "https://kitchenconfidante.com/wp-content/uploads/2015/01/Soothing-Hot-Apple-Berry-Ginger-Juice-www.kitchenconfidante.com-6821-500x500.jpg"
+              text='Berry'
+              path='/products'
+            />
+          </ul>
+          <ul className='cards__items'>
+          <CardItem
+              src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdaSWJpIVlL7NbPUX9MkFRpDV_Ch34YRL1m6QuaS2t5wKYMtVc-49gA1OG8MSp8qDU8OE&usqp=CAU"
+              text='Peach Mango'
+              path='/products'
+            />
+            <CardItem
+              src = "https://www.mysequinedlife.com/wp-content/uploads/2014/11/cranberry-apple-margarita-2.jpg"
+              text='Cran-Apple'
+              path='/products'
+            />
+            <CardItem
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF5Qvcn4ZijRw6K7Cp9xUrcFDvZ-hCrRNOVw&usqp=CAU"
+              text='Fruit Punch'
+              path='/products'
+            />
+            <CardItem
+              src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_IYisTZd0rhbHghiKk8IfC4_El6HdsmMlBw&usqp=CAU"
+              text='White Grape'
+              path='/products'
             />
           </ul>
         </div>
